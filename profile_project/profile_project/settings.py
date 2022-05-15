467ce146353965a9a987982fef2f756aa1de0ff3
NISHANT KUMAR <85857415+nishant-lang@users.noreply.github.com>
2022-05-15 16:42:37 +0530

    Update settings.py

--- profile_project/profile_project/settings.py
+++ profile_project/profile_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z#_#9!aih5eu$ht2!9!*sb!e_lqk@v32+%zi9r(*ch(efvvh23'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
