872e6bc371d13ed251f14d8b64f3446cf546ed7a
NISHANT KUMAR <85857415+nishant-lang@users.noreply.github.com>
2022-05-15 16:56:07 +0530

    Update setup.sh

--- profile_project/deploy/setup.sh
+++ profile_project/deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/nishant-lang/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api/profile_project'
 
 echo "Installing dependencies..."
 apt-get update
